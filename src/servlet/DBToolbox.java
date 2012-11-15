144c42406e170bfc3fa2b738065848c887d3e7c7
henrilahoud <lahoud.henri@gmail.com>
2012-11-15 10:28:30 +0100

    database toolbox

---
+++ src/servlet/DBToolbox.java
@@ -0,0 +1,12 @@
+package servlet;
+
+public abstract class DBToolbox
+{
+	protected static String _dbName = "";
+	protected static DBHandler _dbHandler = null;
+	
+	protected DBToolbox()
+	{
+	}
+	
+}
--- src/servlet/UserBean.java
+++ src/servlet/UserBean.java
@@ -14,21 +14,61 @@ public class UserBean
 	private String _name = "";
 	private String _login = "";
 	private String _password = "";
-	private int _rights = 0;
+	private int _rightTypeId = 0;
+	
+	public int getId() {
+		return _id;
+	}
+	public void setId(int _id) {
+		this._id = _id;
+	}
+	
+	public String getName() {
+		return _name;
+	}
+	public void setName(String name) {
+		this._name = name;
+	}
+	
+	public String getLogin() {
+		return _login;
+	}
+	public void setLogin(String login) {
+		this._login = login;
+	}
+	
+	public String getPassword() {
+		return _password;
+	}
+	public void setPassword(String password) {
+		this._password = password;
+	}
+	
+	public int getRightTypeId() {
+		return _rightTypeId;
+	}
+	public void setRightTypeId(int rightTypeId) {
+		this._rightTypeId = rightTypeId;
+	}
 	
     public UserBean()
     {
         super();
     }
-    public UserBean(int id, String name, String login, String password, int rights)
+    public UserBean(int id, String name, String login, String password, int rightTypeId)
     {
         super();
         
-        _id = id;
-        _name = name;
-        _login = login;
-        _password = password;
-        _rights = rights;
+        setId(id);
+        setName(name);
+        setLogin(login);
+        setPassword(password);
+        setRightTypeId(rightTypeId);
     }
-
+	
+    public boolean checkCredentials (String login, String password)
+    {
+    	return 	DBUserToolbox.isUserValid(login,password);
+    }
+    
 }
