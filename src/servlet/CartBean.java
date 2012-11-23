6409396157b73e91c15c408a08e9312d01e51bb9
adrienrouquet <adrien.rouquet@cpe.fr>
2012-11-23 09:09:45 +0100

    up Cart

--- src/servlet/CartBean.java
+++ src/servlet/CartBean.java
@@ -3,7 +3,7 @@ package servlet;
 import java.util.Map;
 
 public class CartBean {
-	// Map int
+	// Map ProductId:Quantity
 	private Map<Integer, Integer> _mapProduct;
 
 	public Map<Integer, Integer> getMapProduct() {
@@ -14,9 +14,16 @@ public class CartBean {
 		this._mapProduct = mapProduct;
 	}
 
+	public void updateCart(int id, int quantity) {		
+		if (quantity == 0) {
+			deleteFromCart(id);
+			return;
+		}
+		this._mapProduct.put(id, quantity);
+	}
 	
-//	addToCart()
-//	deleteToCart()
-	
+	public void deleteFromCart(int id) {
+		this._mapProduct.remove(id);
+	}
 	
 }
